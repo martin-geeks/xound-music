1694c5b189b77ef2fa81f94bb31b0f0fa459c317
martin-geeks <martin.mt.geek@gmail.com>
2021-07-28 11:47:31 +0200

    more functionalities for deploying

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const moult = require('moult');
 const components = require('./components');
 const log = console.log;
 const app = moult.app('Xound-Music');
+const PORT = process.env.PORT || 5000
 
 app.use(moult.sessions({save: true,json:true,expire:2,path:'/'}))
 app.use(moult.cookies());
@@ -22,4 +23,4 @@ app.routes()
     res.render('root',{});
 })
 
-app.listen(4000);
+app.listen(PORT);
